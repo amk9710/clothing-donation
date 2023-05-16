042991380ee075cb7b7002250fc349718339f310
amk9710 <112566297+amk9710@users.noreply.github.com>
2023-05-16 12:32:27 -0400

    adding location details

--- data/my-points.js
+++ data/my-points.js
@@ -6,200 +6,270 @@ const myPoints =
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9967, 40.7353]
+        "coordinates": [-73.99342025, 40.69462216]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - Brooklyn Heights ",
+        "Type": "Donate",
+        "Address": "150 Montague St, Brooklyn, NY 11201",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 12pm-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9442, 40.6782]
+        "coordinates": [-73.98359322, 40.73998245]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - Gramercy",
+        "Type": "Donate",
+        "Address": "157 E 23rd St, New York, NY 10010",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 11am-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9633, 40.7168]
+        "coordinates": [-73.99721202, 40.74078128]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - Chelsea",
+        "Type": "Donate",
+        "Address": "143 W 17th St, New York, NY 10011",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 11am-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9712, 40.7831]
+        "coordinates": [-73.96176053, 40.76401877]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - 2nd Ave & 64th St",
+        "Type": "Donate",
+        "Address": "1222 2nd Ave, New York, NY 10065",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 11am-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9933, 40.7472]
+        "coordinates": [-73.97783663, 40.77936278]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - Columbus & 74th St",
+        "Type": "Donate",
+        "Address": "306 Columbus Ave, New York, NY 10023",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 11am-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9948, 40.7212]
+        "coordinates": [-73.94961099, 40.78071923]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - Yorkville",
+        "Type": "Donate",
+        "Address": "1730 2nd Ave, New York, NY 10128",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 11am-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9857, 40.6924]
+        "coordinates": [-73.97166037, 40.79544625]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - Broadway & 96th St",
+        "Type": "Donate",
+        "Address": "2569 Broadway, New York, NY 10025",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 11am-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9865, 40.7578]
+        "coordinates": [-74.00591309, 40.7342778]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Housing Works - West Village",
+        "Type": "Donate",
+        "Address": "245 W 10th St, New York, NY 10014",
+        "Collection Details": "New or gently used clothing and shoes ",
+        "Opening Hours": "M-Sa 12pm-7pm, Sun 12pm-5pm",
+        "Link": "https://www.housingworks.org/donate/store-drop-offs"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9396, 40.8219]
+        "coordinates": [-73.99508889, 40.73574344]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Beacon's Closet ",
+        "Type": "Sell",
+        "Address": "10 W 13th St, New York, NY 10011",
+        "Collection Details": "Clothing, shoes and accessories ",
+        "Opening Hours": "M-Sun 11am-8pm",
+        "Link": "https://beaconscloset.com/"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.9825, 40.7685]
+        "coordinates": [-73.9526132, 40.72364032]
       },
       "properties": {
-        "type": "donation "
+        "Name": "Beacon's Closet ",
+        "Type": "Sell",
+        "Address": "74 Guernsey St, Brooklyn, NY 11222",
+        "Collection Details": "Clothing, shoes and accessories ",
+        "Opening Hours": "M-Sun 11am-8pm",
+        "Link": "https://beaconscloset.com/"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.991353, 40.729187]
+        "coordinates": [-73.99484279, 40.68101524]
       },
       "properties": {
-        "type": "recycling "
+        "Name": "Carroll Gardens Greenmarket ",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Sundays, 8am - 1:30pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.977624, 40.751902]
+        "coordinates": [-73.97340589, 40.69145323]
       },
       "properties": {
-        "type": "recycling "
+        "Name": "Ft Green Greenmarket",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Saturdays 8am-1:30pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-74.010314, 40.702663]
+        "coordinates": [-73.96979123, 40.67267824]
       },
       "properties": {
-        "type": "recycling "
+        "Name": "Grand Army Plaza Greenmarket",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Saturdays 8am-2pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.982437, 40.733033]
+        "coordinates": [-73.95243731, 40.71975075]
       },
       "properties": {
-        "type": "recycling "
+        "Name": "McCarren Park Greenmarket",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Saturdays 8:30am-1pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.98958, 40.761254]
+        "coordinates": [-73.97616219, 40.78206849]
       },
       "properties": {
-        "type": "recycling "
+        "Name": "77th/79th Street Greenmarket",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Sundays, 9am-12pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.986026, 40.741631]
+        "coordinates": [-73.96404702, 40.80773962]
       },
       "properties": {
-        "type": "recycling "
+        "Name": "Columbia University Greenmarket",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Sundays, 9am-12pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.957854, 40.708459]
+        "coordinates": [-73.98333909, 40.72624235]
       },
       "properties": {
-        "type": "recycling "
+        "Name": "Tomkins Square Greenmarket",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Sundays, 9am-1pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     },
     {
       "type": "Feature",
       "geometry": {
         "type": "Point",
-        "coordinates": [-73.988272, 40.720271]
+        "coordinates": [-73.92174203, 40.74716516]
       },
       "properties": {
-        "type": "recycling "
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-73.961156, 40.774082]
-      },
-      "properties": {
-        "type": "recycling "
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-73.955385, 40.789295]
-      },
-      "properties": {
-        "type": "recycling "
+        "Name": "Sunnyside Greenmarket",
+        "Type": "Recycle",
+        "Address": "",
+        "Collection Details": "Accepted: Clean and dry clothing, paired shoes, linens, handbags, belts, and other reusable textiles.\nNot accepted: fabric rolls or scraps of any size, rugs, carpeting, pillows, comforters, or luggage.",
+        "Opening Hours": "Saturdays, 9:30am-12:30pm ",
+        "Link": "https://www.grownyc.org/clothing"
       }
     }
   ]
--- js/script.js
+++ js/script.js
@@ -1,95 +1,117 @@
-//['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f']
-// Set up the Mapbox map
-mapboxgl.accessToken = 'pk.eyJ1IjoiYW1rOTcxMCIsImEiOiJjbGc1cWRtNTIwNWl0M2VuNW9yZTJxYmJ2In0.BR49nDMsJOC3F0VxtVqT9Q';
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/amk9710/clhmt6ral00cw01pfajgb1576',
-  center: [-73.9712, 40.7831],
-  zoom: 9.5
-});
+$(document).ready(function() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYW1rOTcxMCIsImEiOiJjbGc1cWRtNTIwNWl0M2VuNW9yZTJxYmJ2In0.BR49nDMsJOC3F0VxtVqT9Q';
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/amk9710/clhmt6ral00cw01pfajgb1576',
+    center: [-73.9712, 40.7831],
+    zoom: 9.5
+  });
 
-let currentFilter = null; // Variable to store the current filter value
+  let currentFilter = null;
 
-
-map.on('load', function () {
-
-  // add the point source and layer
-  map.addSource('my-points', {
+  map.on('load', function () {
+    map.addSource('my-points', {
       type: 'geojson',
       data: 'data/my-points.geojson'
-  })
+    });
 
-  map.addLayer({
-    id: 'circle-my-points',
-    type: 'circle',
-    source: 'my-points',
-    paint: {
-      'circle-radius': 8,
-      'circle-opacity': 0.7,
-      'circle-color': [
-        'match',
-        ['get', 'type'],
-        'donation ', '#33a02c', 
-        'recycling ', '#ff7f00', 
-        '#808080' // Default color if type doesn't match any of the above
-      ]
+    map.addLayer({
+      id: 'circle-my-points',
+      type: 'circle',
+      source: 'my-points',
+      paint: {
+        'circle-radius': [
+          'interpolate', ['linear'], ['zoom'],
+          10, 8,
+          18, 16
+        ],
+        'circle-opacity': 0.7,
+        'circle-color': [
+          'match',
+          ['get', 'Type'],
+          'Donate', '#33a02c',
+          'Recycle', '#ff7f00',
+          'Sell', '#e31a1c',
+          '#808080'
+        ]
       }
-  })
-}
-, )
+    });
 
+    map.on('click', 'circle-my-points', function (e) {
+      var properties = e.features[0].properties;
+      showProperties(properties);
+    });
 
-// Create a geocoder object to use in the search bar
-const geocoder = new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken,
-  mapboxgl: mapboxgl,
-  marker: true,
-  placeholder: 'Search for an address',
-  countries: 'us'
-});
+    map.on('mouseenter', 'circle-my-points', function () {
+      map.getCanvas().style.cursor = 'pointer';
+    });
 
-// Add the geocoder to the map
-$('#sidebar').append(geocoder.onAdd(map));
+    map.on('mouseleave', 'circle-my-points', function () {
+      map.getCanvas().style.cursor = '';
+    });
 
-// Add navigation controls to map
-map.addControl(new mapboxgl.NavigationControl());
+    function showProperties(properties) {
+      // Clear the previous properties
+      $('#sidebar #content').empty();
+    
+      // Create and append the property elements
+      var name = properties['Name'];
+      var nameElement = $('<p>').addClass('property').text(name).css('font-weight', 'bold');
+      $('#sidebar #content').append(nameElement);
+    
+      var openingHours = properties['Opening Hours'];
+      if (openingHours) {
+        var openingHoursElement = $('<p>').addClass('property').text('Opening Hours: ' + openingHours);
+        $('#sidebar #content').append(openingHoursElement);
+      }
 
+      var collectionDetails = properties['Collection Details'];
+      if (collectionDetails) {
+        var collectionDetailsElement = $('<p>').addClass('property').text('Collection Details: ' + collectionDetails);
+        $('#sidebar #content').append(collectionDetailsElement);
+      }
+    
+      var address = properties['Address'];
+      if (address) {
+        var addressElement = $('<p>').addClass('property').html('Address: <a href="https://www.google.com/maps/search/?api=1&query=' + encodeURIComponent(address) + '" target="_blank">' + address + '</a>');
+        $('#sidebar #content').append(addressElement);
+      }
+    
+      var link = properties['Link'];
+      if (link) {
+        var linkElement = $('<p>').addClass('property').html(' <a href="' + link + '" target="_blank">' + link + '</a>');
+        $('#sidebar #content').append(linkElement);
+      }
+    }
+    
 
-function filterPoints(type) {
-  if (type === currentFilter) return; // If the same filter is already applied, do nothing
+    $('#showDonationButton').on('click', function () {
+      filterPoints('Donate');
+    });
 
-  currentFilter = type; // Update the current filter value
+    $('#showRecycleButton').on('click', function () {
+      filterPoints('Recycle');
+    });
 
-  // Update the filter on the layer
-  map.setFilter('circle-my-points', ['==', 'type', type]);
-}
+    $('#showSellButton').on('click', function () {
+      filterPoints('Sell');
+    });
 
-// Attach click event listener to the button
-$('#showDonationButton').on('click', function () {
-  filterPoints('donation ');
-});
+    $('#clearFilterButton').on('click', function () {
+      clearFilter();
+    });
 
-$('#showRecycleButton').on('click', function () {
-  filterPoints('recycling ');
-});
+    function filterPoints(Type) {
+      if (Type === currentFilter) return;
+      currentFilter = Type;
+      map.setFilter('circle-my-points', ['==', 'Type', Type]);
+    }
 
-// Attach click event listener to the "Clear Filter" button
-$('#clearFilterButton').on('click', function () {
-  clearFilter();
-});
-
-function clearFilter() {
-  if (currentFilter !== null) {
-    // Remove the filter on the layer
-    map.setFilter('circle-my-points', ['has', 'type']);
-    currentFilter = null; // Reset the current filter value
-  }
-}
-
-
-map.on('mouseenter', 'circle-my-points', function () {
-  map.getCanvas().style.cursor = 'pointer';
-});
-map.on('mouseleave', 'circle-my-points', function () {
-  map.getCanvas().style.cursor = '';
+    function clearFilter() {
+      if (currentFilter !== null) {
+        map.setFilter('circle-my-points', ['has', 'Type']);
+        currentFilter = null;
+      }
+    }
+  });
 });
